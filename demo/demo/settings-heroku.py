6b2adba9e44ea60635c16787c1c2bcde695ebaee
Frédéric Ravetier <frederic.ravetier@ametys.org>
2021-12-29 19:11:54 +0100

    heroku deploy

--- demo/demo/settings-heroku.py
+++ demo/demo/settings-heroku.py
@@ -129,13 +129,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-print(PROJECT_ROOT)
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+print("PROJECT_ROOT = " % BASE_DIR)
+STATIC_ROOT  =   os.path.join(PROJECT_DIR , 'staticfiles')
 STATIC_URL = '/static/'
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_DIR , 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
